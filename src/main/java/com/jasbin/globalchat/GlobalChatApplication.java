4fc9562e21f729c017812da4860348f2e6e36d57
jasbin <jasbinkarki@gmail.com>
2019-01-28 21:20:18 +0545

    chat app using username only

---
+++ src/main/java/com/jasbin/globalchat/GlobalChatApplication.java
@@ -0,0 +1,14 @@
+package com.jasbin.globalchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalChatApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jasbin/globalchat/GlobalChatApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jasbin.globalchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlobalChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
